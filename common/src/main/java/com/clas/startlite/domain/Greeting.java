e104e63f81b88242b85afa0be284f22be8daed4d
Son Nguyen <son.nguyen.thanh@live.com>
2014-08-13 08:52:57 +0700

    init

---
+++ common/src/main/java/com/clas/startlite/domain/Greeting.java
@@ -0,0 +1,22 @@
+package com.clas.startlite.domain;
+
+/**
+ * Created by Son on 8/13/14.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ web/src/main/java/com/clas/startlite/webapp/controller/GreetingController.java
@@ -0,0 +1,10 @@
+package com.clas.startlite.webapp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Son on 8/13/14.
+ */
+@RestController
+public class GreetingController {
+}
